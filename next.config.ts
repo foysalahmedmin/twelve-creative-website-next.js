4c2f930a4108a4b3273ff2d915cf910d80ffd284
Foysal Ahmed <foysalahmedmin@gmail.com>
2026-05-25 03:27:19 +0600

    chore(config): allow all external image domains in Next.js remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,40 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['192.168.0.110'],
   images: {
-
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "sasthyaseba.com",
+        hostname: "**",
       },
       {
-        protocol: "https",
-        hostname: "*.sasthyaseba.com",
+        protocol: "http",
+        hostname: "**",
       },
-      {
-        protocol: "https",
-        hostname: "**.sasthyaseba.com",
-      },
-      {
-        protocol: "https",
-        hostname: "developer.android.com",
-      },
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "doctime.com.bd"
-      },
-      {
-        protocol: "https",
-        hostname: "admin.twelvecreative.io"
-      }
     ],
   },
 };
